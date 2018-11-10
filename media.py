e2069f515a68febf2d1ab17d795a4ca39004af9e
Gaurav Ahuja <gauravahuja.becse16@pec.edu.in>
2018-11-10 09:29:38 +0530

    Add files via upload

--- media.py
+++ media.py
@@ -1,9 +1,10 @@
-#Creating a class Movie, that serves as the blueprint
+# Creating a class Movie, that serves as the blueprint
+
 
 class Movie():
-    #__init__ is the constructor and initializes the instance variables
-    def __init__(self,movie_title,poster_image,trailer_youtube):
-        self.title=movie_title
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube
-        #self refers to pointing to the current instance
+    # __init__ is the constructor and initializes the instance variables
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        '''self refers to pointing to the current instance'''
